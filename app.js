62c0c1cf383d6cd29f788d9e2a6e940b1f9ea287
Arkadyuti <bandyopadhyayarkadyuti@gmail.com>
2020-07-29 01:52:12 +0530

    Made minor changes for fixing CORS issues'

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req,res,next) => {
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Headers','Origin,X-Requested-With, Content-Type, Access, Authorization');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST');
+  next();
+});
+
 app.use('/',registration);
 
 app.use((req, res, next) => {
