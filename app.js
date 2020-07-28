a2481638f305a6cff862f46e037d3fd311603266
Arkadyuti <bandyopadhyayarkadyuti@gmail.com>
2020-07-29 02:11:25 +0530

    More CORS bug fixes

--- app.js
+++ app.js
@@ -8,14 +8,12 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(bodyParser.json());
-
 app.use((req,res,next) => {
   res.setHeader('Access-Control-Allow-Origin','*');
-  res.setHeader('Access-Control-Allow-Headers','Origin,X-Requested-With, Content-Type, Access, Authorization');
-  res.setHeader('Access-Control-Allow-Methods','GET,POST');
   next();
 });
+
+app.use(bodyParser.json());
 
 app.use('/',registration);
 
